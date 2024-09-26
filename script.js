446b8b67695181f9d02b2c2479d8f729f211bea9
marcoscaldas <donarumo@hotmail.com>
2024-09-26 11:24:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var a = 10;
+
+var nome = "Marcos";
+
+var total = a - nome;
+
+console.log(total);
---
+++ while.js
@@ -0,0 +1,12 @@
+var numero = 1;
+
+while( numero <= 5  ){
+
+    console.log("Etapa ", numero);
+
+    numero ++
+
+}
+
+
+
